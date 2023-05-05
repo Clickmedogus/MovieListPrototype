91b2dfd1685e267c4c3f14fcfa165b0538198699
Clickmedogus <dizgi@clickmelive.com>
2023-05-05 16:27:25 +0300

    The design of the list has been edited. Necessary corrections have been made.

--- app/src/main/java/com/example/movielistprototype/MainActivity.kt
+++ app/src/main/java/com/example/movielistprototype/MainActivity.kt
@@ -11,29 +11,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.movielistprototype.ui.theme.MovieListPrototypeTheme
 import com.example.movielistprototype.utils.Resource
-import com.example.movielistprototype.view.UserListItem
+import com.example.movielistprototype.view.PeopleListItem
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @AndroidEntryPoint
@@ -53,7 +51,6 @@ class MainActivity : ComponentActivity() {
 fun CallApi(
     viewModel: PeopleViewModel = hiltViewModel()
 ) {
-    val scope = rememberCoroutineScope()
     val context = LocalContext.current
     val scaffoldState = rememberScaffoldState()
     val getAllUserData = viewModel.getUserData
@@ -116,7 +113,7 @@ fun CallApi(
                             modifier = Modifier.padding(10.dp)
                         ) {
                             items(getAllUserData.value!!.size) { index ->
-                                UserListItem(getAllUserData.value!![index])
+                                PeopleListItem(getAllUserData.value!![index])
 
                             }
                         }
--- app/src/main/java/com/example/movielistprototype/view/peopleListItem.kt
+++ app/src/main/java/com/example/movielistprototype/view/peopleListItem.kt
@@ -1,9 +1,13 @@
 package com.example.movielistprototype.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -12,14 +16,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.movielistprototype.R
 import com.example.movielistprototype.data.model.People
 
+@Preview
 @Composable
-fun UserListItem(people: People) {
+fun PeopleListItem(@PreviewParameter(PeoplePreviewParameterProvider :: class) people: People) {
 
     Card(
         modifier = Modifier
@@ -35,32 +46,59 @@ fun UserListItem(people: People) {
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = people.name.toString(),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = FontFamily.SansSerif
-            )
 
-            Spacer(modifier = Modifier.padding(10.dp))
+            Row {
 
-            Text(
-                text = "Gender: ${people.gender}",
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Normal,
-                fontFamily = FontFamily.SansSerif
-            )
+                Image(
+                    painter = painterResource(id = R.drawable.human),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(110.dp)
+                        .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+                )
 
-            Spacer(modifier = Modifier.padding(5.dp))
+                Column(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                        .align(Alignment.CenterVertically)) {
+                    Text(
+                        text = people.name,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.SansSerif
+                    )
 
-            Text(
-                text = "Name: ${people.eyeColor}",
-                modifier = Modifier.fillMaxWidth(),
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Normal,
-                fontFamily = FontFamily.SansSerif
-            )
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    Text(
+                        text = "Gender: ${people.gender}",
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = FontFamily.SansSerif
+                    )
+
+                    Spacer(modifier = Modifier.padding(5.dp))
+
+                    Text(
+                        text = "Hair Color: ${people.hairColor}",
+                        modifier = Modifier.fillMaxWidth(),
+                        fontSize = 17.sp,
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = FontFamily.SansSerif
+                    )
+                }
+            }
         }
     }
+
+}
+
+class PeoplePreviewParameterProvider : PreviewParameterProvider<People> {
+    override val values = sequenceOf(
+        People("DOĞUŞ İZGİ","180","100","male","black")
+    )
 }
