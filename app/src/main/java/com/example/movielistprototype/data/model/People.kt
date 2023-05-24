783c6d14054f8a63221a156df05a2dec9b38ff87
Clickmedogus <dizgi@clickmelive.com>
2023-05-24 12:44:52 +0300

    People.kt fixed.

--- app/src/main/java/com/example/movielistprototype/data/model/People.kt
+++ app/src/main/java/com/example/movielistprototype/data/model/People.kt
@@ -1,37 +1,12 @@
 package com.example.movielistprototype.data.model
 
-import android.os.Parcelable
-import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
 
-
-@Parcelize
 data class People(
-    @SerializedName("name")
-    val name: String = " ",
-
-    @SerializedName("height")
-    val height: String = " ",
-
-    @SerializedName("mass")
-    val mass: String = " ",
-
-    @SerializedName("gender")
-    val gender: String = " ",
-
-    @SerializedName("hair_color")
-    val hairColor: String = " ",
-
-    @SerializedName("skin_color")
-    val skinColor: String = " ",
-
-    @SerializedName("eye_color")
-    val eyeColor: String = " "
-) : Parcelable
-{
-
-    companion object {
-        // eşlik eden nesnenin özellikleri ve metotları burada tanımlanır
-    }
-
-}
+    val name: String = "",
+    val height: String = "",
+    val mass: String = "",
+    val gender: String = "",
+    val hairColor: String = "",
+    val skinColor: String = "",
+    val eyeColor: String = ""
+)
